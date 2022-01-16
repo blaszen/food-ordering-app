5fdbb4c2e4822e6ecbd1c533bfb700d3c116d5ae
blaszen <blaszen.dev@gmail.com>
2022-01-15 16:49:16 -0800

    nextjs app

---
+++ component/Navbar.jsx
@@ -0,0 +1,13 @@
+import styles from '../styles/Home.module.css'
+
+
+
+const Navbar = () => {
+    return (
+        <div className={styles.container}>
+            navbar
+        </div>
+    )
+}
+
+export default Navbar
